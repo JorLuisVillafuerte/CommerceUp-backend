04028370ade8febc71ce0456dfacbd367096f340
Jorge <jorl.villafuerte@gmail.com>
2020-09-22 18:07:36 -0300

    order fix

--- src/main/java/ar/com/commerceup/domain/Order.java
+++ src/main/java/ar/com/commerceup/domain/Order.java
@@ -1,66 +1,147 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package ar.com.commerceup.domain;
 
 import java.io.Serializable;
 import java.util.Date;
 import javax.persistence.Basic;
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
-import javax.validation.constraints.NotBlank;
+import javax.persistence.TemporalType;
 import javax.validation.constraints.Size;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
+import javax.xml.bind.annotation.XmlRootElement;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
+
 @Entity
-@Table(name="order")
-public class Order implements Serializable{
+@Table(name = "orderSale")
+public class Order implements Serializable {
+
     private static final long serialVersionUID = 1L;
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Basic(optional = false)
     private Integer internalid;
-    @Column(unique = true)
-    @NotBlank(message="El codigo de categoria no puede ser nulo/vacio.")
-    @Size(max = 10, message="El codigo de categoria debe ser menor a 10 caracteres.")
+    @Size(max = 10)
     private String orderCode;
-    @Size(max = 255, message="Se excedio del maximo de caracteres permitidos.")
+    @Size(max = 200)
     private String description;
-    @Column(updatable=false)
-    @CreationTimestamp
-    @Temporal(javax.persistence.TemporalType.DATE)
+    @Temporal(TemporalType.TIMESTAMP)
     private Date dateCreated;
-    @UpdateTimestamp
-    @Temporal(javax.persistence.TemporalType.DATE)
+    @Temporal(TemporalType.TIMESTAMP)
     private Date dateModified;
-    @Column(updatable=false)
-    @CreationTimestamp
-    @Temporal(javax.persistence.TemporalType.DATE)
+    @Temporal(TemporalType.TIMESTAMP)
     private Date dateSale;
-    @Size(max = 50, message="Se excedio del maximo de caracteres permitidos.")
+    @Size(max = 50)
     private String salePlatform;
-    
-    @JoinColumn(name = "usuarioId", referencedColumnName = "internalid")
-    @ManyToOne
-    private User usuarioId;
     @JoinColumn(name = "orderDetailId", referencedColumnName = "internalid")
-    @OneToOne
-    private OrderDetail orderDetailId;
-    @JoinColumn(name = "statusId", referencedColumnName = "internalid")
     @ManyToOne
-    private Status statusId;
-    
+    private OrderDetail orderDetailId;
+
+    public Order() {
+    }
+
+    public Order(Integer internalid) {
+        this.internalid = internalid;
+    }
+
+    public Integer getInternalid() {
+        return internalid;
+    }
+
+    public void setInternalid(Integer internalid) {
+        this.internalid = internalid;
+    }
+
+    public String getOrderCode() {
+        return orderCode;
+    }
+
+    public void setOrderCode(String orderCode) {
+        this.orderCode = orderCode;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(Date dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+
+    public Date getDateModified() {
+        return dateModified;
+    }
+
+    public void setDateModified(Date dateModified) {
+        this.dateModified = dateModified;
+    }
+
+    public Date getDateSale() {
+        return dateSale;
+    }
+
+    public void setDateSale(Date dateSale) {
+        this.dateSale = dateSale;
+    }
+
+    public String getSalePlatform() {
+        return salePlatform;
+    }
+
+    public void setSalePlatform(String salePlatform) {
+        this.salePlatform = salePlatform;
+    }
+
+    public OrderDetail getOrderDetailId() {
+        return orderDetailId;
+    }
+
+    public void setOrderDetailId(OrderDetail orderDetailId) {
+        this.orderDetailId = orderDetailId;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (internalid != null ? internalid.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Order)) {
+            return false;
+        }
+        Order other = (Order) object;
+        if ((this.internalid == null && other.internalid != null) || (this.internalid != null && !this.internalid.equals(other.internalid))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "ar.com.commerceup.domain.Order1[ internalid=" + internalid + " ]";
+    }
     
 }
--- src/main/java/ar/com/commerceup/domain/OrderDetail.java
+++ src/main/java/ar/com/commerceup/domain/OrderDetail.java
@@ -1,36 +1,87 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package ar.com.commerceup.domain;
 
 import java.io.Serializable;
 import java.util.List;
 import javax.persistence.Basic;
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
 import javax.persistence.Table;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Size;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
+/**
+ *
+ * @author MR ROBOT
+ */
 @Entity
-@Table(name="orderDetail")
-public class OrderDetail implements Serializable{
-    
+@Table(name = "orderDetail")
+public class OrderDetail implements Serializable {
+
     private static final long serialVersionUID = 1L;
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Basic(optional = false)
     private Integer internalid;
-    @OneToOne
-    private Order orderId;
-    @OneToMany
-    private List<Product> productId;
+    @OneToMany(mappedBy = "orderDetailId")
+    private List<Order> order1List;
+
+    public OrderDetail() {
+    }
+
+    public OrderDetail(Integer internalid) {
+        this.internalid = internalid;
+    }
+
+    public Integer getInternalid() {
+        return internalid;
+    }
+
+    public void setInternalid(Integer internalid) {
+        this.internalid = internalid;
+    }
+
+    @XmlTransient
+    public List<Order> getOrder1List() {
+        return order1List;
+    }
+
+    public void setOrder1List(List<Order> order1List) {
+        this.order1List = order1List;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (internalid != null ? internalid.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof OrderDetail)) {
+            return false;
+        }
+        OrderDetail other = (OrderDetail) object;
+        if ((this.internalid == null && other.internalid != null) || (this.internalid != null && !this.internalid.equals(other.internalid))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "ar.com.commerceup.domain.OrderDetail[ internalid=" + internalid + " ]";
+    }
+    
 }
